8259f124c291e1c87e8f118e09595a1d257f9b54
James Von Vreckin <jcvonv01@louisville.edu>
2017-10-19 17:48:07 -0400

    Add project files.

---
+++ Family_Feud/Controllers/FamilyFeudController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Family_Feud.Controllers
+{
+    public class FamilyFeudController : Controller
+    {
+        // GET: FamilyFeud
+        public ActionResult StartPage()
+        {
+            return View();
+        }
+        public ActionResult FamilyPicker()
+        {
+            return View();
+        }
+        public ActionResult GameBoard()
+        {
+            return View();
+        }
+    }
+}
