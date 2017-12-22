58909137d05ffed79e6ccb97c5cd9001cb3e0f84
James Von Vreckin <jcvonv01@louisville.edu>
2017-12-21 23:18:35 -0500

    Revert "implemented a QR code generator that is used to display a QR code on the Gameboard. Users can also scan the code and it will direct them to Steves Screen"
    This reverts commit f97511da017553e0ac9cadfe5e9a60f18d84388a.

--- Family_Feud/Controllers/FamilyFeudController.cs
+++ Family_Feud/Controllers/FamilyFeudController.cs
@@ -9,8 +9,6 @@ namespace Family_Feud.Controllers
 {
     public class FamilyFeudController : Controller
     {
-        const string _pubKey = "pub-c-12219e16-c45b-411e-aa5a-977e1a130441";
-        const string _subKey = "sub-c-e53da4be-c647-11e7-8d13-12daa3930087";
         // GET: FamilyFeud
         public ActionResult StartPage()
         {
@@ -26,32 +24,16 @@ namespace Family_Feud.Controllers
         }
         public ActionResult DavidsGameBoard(string team1, string team2)
         {
-            GameData gameData = new GameData();
+            PlayerInfo teamNames = new PlayerInfo();
+            teamNames.TeamOne = team1;
+            teamNames.TeamTwo = team2;
 
-            gameData.TeamOne = team1;
-            gameData.TeamTwo = team2;
-
-            gameData.PubKey = _pubKey;
-            gameData.SubKey = _subKey;
-
-            gameData.Channel = gameData.GenerateChannelName();
-
-            return View(gameData);
+            return View(teamNames);
         }
 
-        public ActionResult SteveScreen(string team1, string team2,  string channel)
+        public ActionResult SteveScreen()
         {
-            GameData data = new GameData();
-
-            data.TeamOne = team1;
-            data.TeamTwo = team2;
-
-            data.PubKey = _pubKey;
-            data.SubKey = _subKey;
-
-            data.Channel = channel;
-
-            return View(data);
+            return View();
         }
     }
 }
